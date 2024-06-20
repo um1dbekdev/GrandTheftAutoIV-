51997a0ebaf1247be17416b50a1d4fa48ea2c0b4
um1dbekdev <umidbekdzumabaev@gmail.com>
2024-06-20 22:40:17 +0500

    gta4

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,6 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <h1 className='font-[900] text-[red] '>salom bu saytnio indi boshladik</h1>
+    <h1 className='font-[900] text-[red] '>salom bu saytnio indi boshladiq</h1>
   </React.StrictMode>,
 )
